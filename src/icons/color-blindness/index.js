6ce5685c7310e3aff0e679ad5a680093aa88f11c
Caleb Nance <calebnance@gmail.com>
2023-05-12 11:59:11 -0400

    added svgs for color blindness filter selection

---
+++ src/icons/color-blindness/index.js
@@ -0,0 +1,19 @@
+import SvgAchromatomaly from './SvgAchromatomaly';
+import SvgAchromatopsia from './SvgAchromatopsia';
+import SvgDeuteranomaly from './SvgDeuteranomaly';
+import SvgDeuteranopia from './SvgDeuteranopia';
+import SvgOriginal from './SvgOriginal';
+import SvgProtanopia from './SvgProtanopia';
+import SvgProtonomaly from './SvgProtonomaly';
+import SvgTritanopia from './SvgTritanopia';
+
+export default {
+  SvgAchromatomaly,
+  SvgAchromatopsia,
+  SvgDeuteranomaly,
+  SvgDeuteranopia,
+  SvgOriginal,
+  SvgProtanopia,
+  SvgProtonomaly,
+  SvgTritanopia
+};
