e521d1631515ca2e3b8750dc305c302565402362
Caleb Nance <calebnance@gmail.com>
2024-09-18 21:25:19 -0400

    complex gesture: update name and ordering

--- src/data/gesture-types.js
+++ src/data/gesture-types.js
@@ -7,18 +7,18 @@ import SvgGestures from '../icons/complex-gestures';
 export default {
   swipe: {
     id: '1',
-    label: 'swipe',
+    label: 'swipe/slide/drag',
     icon: <SvgGestures.SvgSwipe />
   },
-  'pinch-and-zoom': {
-    id: '2',
-    label: 'pinch & zoom',
-    icon: <SvgGestures.SvgPinchZoom />
-  },
   'drag-and-drop': {
-    id: '3',
+    id: '2',
     icon: <SvgGestures.SvgDragDrop />,
     label: 'drag & drop'
+  },
+  'pinch-and-zoom': {
+    id: '3',
+    label: 'pinch & zoom',
+    icon: <SvgGestures.SvgPinchZoom />
   },
   rotate: {
     id: '4',
