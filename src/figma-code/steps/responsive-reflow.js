f48ee0f1f7e5ce56d7777c08b3cb3a707ce53903
Caleb Nance <calebnance@gmail.com>
2024-09-19 12:43:22 -0400

    when creating Responsive reflow screens, accounts for Text resizing location

--- src/figma-code/steps/responsive-reflow.js
+++ src/figma-code/steps/responsive-reflow.js
@@ -1,14 +1,46 @@
 import config from '../config';
 
 export const createResponsiveDesigns = (msg) => {
+  const { currentPage } = figma;
   const { breakpoints, page } = msg;
   const { id: pageId } = page;
 
   // get selected page node
   const pageNode = figma.getNodeById(pageId);
+  let startFromX = pageNode.x + pageNode.width + config.annotationWidth;
+
+  // does text resizing page exist?
+  let hasTextResizingPage = null;
+  const checkName = (name) => {
+    const check = `${pageNode.name} Text Zoom`;
+
+    return name.startsWith(check);
+  };
+
+  // loop through all high level pages and section pages
+  currentPage.children.forEach((topLevel) => {
+    const { name, type } = topLevel;
+
+    // account for sections
+    if (type === 'SECTION') {
+      // loop through section pages
+      topLevel.children.forEach((sectionPage) => {
+        if (checkName(sectionPage.name)) {
+          hasTextResizingPage = sectionPage;
+        }
+      });
+    } else if (checkName(name)) {
+      hasTextResizingPage = topLevel;
+    }
+  });
+
+  // if Text Zoom page exists, get dimensions
+  if (hasTextResizingPage !== null) {
+    startFromX = hasTextResizingPage.x + hasTextResizingPage.width;
+  }
 
   const gutterSpace = 124;
-  let startX = pageNode.x + pageNode.width + config.annotationWidth;
+  let startX = startFromX;
 
   const createdScreens = [];
   breakpoints.forEach((screenSpec) => {
