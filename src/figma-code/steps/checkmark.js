cda177e0af88d79744211e3a271a7d343cb2cd4a
Caleb Nance <calebnance@gmail.com>
2024-09-19 12:51:12 -0400

    Bug fix: errors occurred when checkbox confirmed on Responsive reflow

--- src/figma-code/steps/checkmark.js
+++ src/figma-code/steps/checkmark.js
@@ -19,75 +19,92 @@ export const add = (msg) => {
   let status = 'add';
   let layerId = null;
 
-  // if layer exist and create is false, delete it
-  if (layerExists !== null && create === false) {
-    const oldAltTextFrame = figma.getNodeById(layerExists);
-    // https://www.figma.com/plugin-docs/api/properties/nodes-remove/
-    oldAltTextFrame.remove();
+  try {
+    // if layer exist and create is false, delete it
+    if (layerExists !== null && create === false) {
+      const oldAltTextFrame = figma.getNodeById(layerExists);
+      // https://www.figma.com/plugin-docs/api/properties/nodes-remove/
+      oldAltTextFrame.remove();
 
-    status = 'remove';
+      status = 'remove';
 
-    // if no more children in main A11y layer, delete the layer
-    // if (mainFrame.children.length === 0) {
-    //   mainFrame.remove();
-    // }
-  } else if (create) {
-    if (layerExists === null) {
-      // create the layer frame
-      const layerFrame = figmaLayer.createTransparentFrame({
-        name: layerName,
-        height: 10,
-        width: 10
-      });
+      // if no more children in main A11y layer, delete the layer
+      // if (mainFrame.children.length === 0) {
+      //   mainFrame.remove();
+      // }
+    } else if (create) {
+      if (layerExists === null) {
+        // create the layer frame
+        const layerFrame = figmaLayer.createTransparentFrame({
+          name: layerName,
+          height: 10,
+          width: 10
+        });
 
-      // update with id (for future scanning)
-      layerFrame.name = `${layerName} | ${layerFrame.id}`;
-      layerFrame.expanded = false;
-      layerFrame.visible = false;
+        // update with id (for future scanning)
+        layerFrame.name = `${layerName} | ${layerFrame.id}`;
+        layerFrame.expanded = false;
+        layerFrame.visible = false;
 
-      layerId = layerFrame.id;
+        layerId = layerFrame.id;
 
-      // add within main Accessibility layer
-      mainFrame.appendChild(layerFrame);
-    } else {
-      layerId = layerExists;
-    }
-  }
-
-  // grab main page a11y scan was for
-  const { currentPage } = figma;
-  const [originalPage] = currentPage.children.filter(
-    (c) => c.name === saniName
-  );
-
-  // update pagesData
-  const stepKey = layerName.replace(/ Layer/g, '');
-
-  const updateData = { id: layerId };
-  if (existingData) {
-    updateData.existingData = existingData;
-  }
-
-  figma.ui.postMessage({
-    type: 'update-pages-data',
-    data: {
-      status,
-      stepKey,
-      stateKey,
-      [stepKey]: updateData,
-      main: {
-        id: mainFrame.id,
-        name: saniName,
-        pageId: originalPage.id,
-        page: {
-          bounds,
-          id: originalPage.id,
-          mainPageId,
-          name: saniName
-        }
+        // add within main Accessibility layer
+        mainFrame.appendChild(layerFrame);
+      } else {
+        layerId = layerExists;
       }
     }
-  });
+
+    const { currentPage } = figma;
+
+    // grab main page a11y scan was for
+    let originalPage = null;
+    currentPage.children.forEach((topLevel) => {
+      // account for sections
+      if (topLevel.type === 'SECTION') {
+        // loop through section pages
+        topLevel.children.forEach((sectionPage) => {
+          if (sectionPage.name === saniName) {
+            originalPage = sectionPage;
+          }
+        });
+      } else if (topLevel.name === saniName) {
+        originalPage = topLevel;
+      }
+    });
+
+    // update pagesData
+    const stepKey = layerName.replace(/ Layer/g, '');
+
+    const updateData = { id: layerId };
+    if (existingData) {
+      updateData.existingData = existingData;
+    }
+
+    figma.ui.postMessage({
+      type: 'update-pages-data',
+      data: {
+        status,
+        stepKey,
+        stateKey,
+        [stepKey]: updateData,
+        main: {
+          id: mainFrame.id,
+          name: saniName,
+          pageId: originalPage.id,
+          page: {
+            bounds,
+            id: originalPage.id,
+            mainPageId,
+            name: saniName
+          }
+        }
+      }
+    });
+  } catch (err) {
+    console.log('ERROR :: step.checkmark.add()');
+    console.log(err);
+  }
 };
 
 export default { add };
