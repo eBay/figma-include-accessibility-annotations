b5638855f15b7c877c55418bb89b8d0b52b61d9b
Caleb Nance <calebnance@gmail.com>
2024-09-18 21:09:18 -0400

    Banner Tip now allows for expand/collapse functionality

--- src/components/BannerTipText/index.js
+++ src/components/BannerTipText/index.js
@@ -1,22 +1,52 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { utils } from '../../constants';
+
+// icons
+import { SvgChevronDown } from '../../icons';
 
 // styles
 import './styles.scss';
 
 function BannerTipText(props) {
-  const { footer = null, helpText = 'Learn more', helpUrl = null } = props;
-  const { text } = props;
+  const { expandedDefault = true, footer = null } = props;
+  const { helpText = 'Learn more', helpUrl = null, text } = props;
+
+  // local state
+  const [expanded, setExpanded] = React.useState(expandedDefault);
+
+  // ui state
+  const ariaLabel = expanded ? 'collapse' : 'expand';
+  const rotateClass = expanded ? 'rotate-right-rev' : 'rotate-left-rev';
+  const tipTextClass = expanded ? '' : 'tip-text-collapsed';
 
   return (
     <div className="banner-tip">
       <div className="flex-row align-start">
         <div className="tip-label">tip</div>
 
-        <p dangerouslySetInnerHTML={{ __html: text }} />
+        <p
+          className={tipTextClass}
+          dangerouslySetInnerHTML={{ __html: text }}
+        />
+
+        <div
+          aria-label={`${ariaLabel} tip`}
+          className="tip-toggle"
+          onClick={() => setExpanded(!expanded)}
+          onKeyDown={({ key }) => {
+            if (utils.isEnterKey(key)) setExpanded(!expanded);
+          }}
+          role="button"
+          tabIndex="0"
+        >
+          <div className={`svg-theme animated ${rotateClass}`}>
+            <SvgChevronDown size={12} />
+          </div>
+        </div>
       </div>
 
-      {footer && footer}
+      {footer && expanded && footer}
 
       {helpUrl !== null && (
         <a className="tip-link" href={helpUrl} target="_blank" rel="noreferrer">
@@ -32,6 +62,7 @@ BannerTipText.propTypes = {
   text: PropTypes.string.isRequired,
 
   // optional
+  expandedDefault: PropTypes.bool,
   footer: PropTypes.element,
   helpText: PropTypes.string,
   helpUrl: PropTypes.string
